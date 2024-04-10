74810eb4df8eef58271d2e20c09d6a5948134c33
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2024-04-10 14:01:19 +0300

    status

--- app.js
+++ app.js
@@ -9,5 +9,15 @@ app.use(serverLog);
 app.get("/", (req, res) => {
     res.send("<h1>Home</h1>")
 });
+app.use((req, res) => {
+    res.status(404).json({ message: 'Not found' })
+})
 
-app.listen(4000, ()=>console.log("Server running"))
+app.use((err, req, res, next) => {
+    const { status = 500, message = 'Server error' } = err;
+    res.status(status).json({
+        message,
+    });
+});
+
+app.listen(4000)
